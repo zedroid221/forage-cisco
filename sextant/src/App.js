d2f7d92bad9b135490063175808dc03196c5bfd8
tachyon420 <pa6goian@ags.bucks.sch.uk>
2022-07-31 01:17:12 +0100

    added banner and exhibit components

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Banner extends React.Component{
+  render() {
+    return (
+      <h1 className="title">{this.props.name}</h1>
+    );
+  }
+}
+
+class Exhibit extends React.Component {
+  render() {
+    return (
+      <div className="exhibit">
+        <Banner name={this.props.title}/>
+        <div className="datapoint">
+          <h6>datapoint</h6>
+        </div>
+      </div>
+      
+    );
+  };
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner name="Sextant" className/>
+      <Exhibit title="hello"/>
     </div>
   );
 }
